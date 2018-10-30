4efac2495183eb44a2875605d2750fd1bfcac318
Andrew Smith <gingermoose90@gmail.com>
2018-10-31 03:39:32 +1100

    implemented automated test for first bug

--- test/hotel/checkout/TestBugOne.java
+++ test/hotel/checkout/TestBugOne.java
@@ -1,16 +1,26 @@
 package hotel.checkout;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+
+import hotel.checkin.CheckinCTL;
+import hotel.checkin.CheckinUI;
 import hotel.credit.CreditCard;
 import hotel.credit.CreditCardType;
 import hotel.entities.Booking;
@@ -18,16 +28,30 @@ import hotel.entities.Guest;
 import hotel.entities.Hotel;
 import hotel.entities.Room;
 import hotel.entities.RoomType;
+import hotel.entities.ServiceCharge;
+import hotel.entities.ServiceType;
+import hotel.service.RecordServiceCTL;
+import hotel.service.RecordServiceUI;
 
+@ExtendWith(MockitoExtension.class)
 class TestBugOne {
+	
+	//@Mock CheckoutUI checkoutUi;
+	CheckinUI checkinUi;
+	CheckoutUI checkoutUi;
+	RecordServiceUI recordServiceUi;
 	
 	Hotel hotel;
 	Booking booking;
 	Guest guest;
 	Room room;
-	CreditCard creditcard;
+	CreditCard creditCard;
 	CreditCardType cardType;
-	CheckoutCTL control;
+	CheckinCTL checkInControl;
+	CheckoutCTL checkOutControl;
+	RecordServiceCTL recordServiceControl;
+	ServiceCharge serviceCharge;
+	ServiceType serviceType;
 	
 	Date arrivalDate;
 	static SimpleDateFormat format;
@@ -40,6 +64,7 @@ class TestBugOne {
 	String guestAddress;
 	int phoneNumber;
 	int ccv;
+	double cost;
 	
 	static final int AUTHORISED_CARD_NUMBER = 3;
 	
@@ -56,18 +81,47 @@ class TestBugOne {
 	@BeforeEach
 	void setUp() throws Exception {
 		hotel = new Hotel();
-		control = new CheckoutCTL(hotel);
+		checkOutControl = new CheckoutCTL(hotel);
+		checkOutControl.checkoutUI = new CheckoutUI(checkOutControl);
+		
+		checkInControl = new CheckinCTL(hotel);
+		checkInControl.checkInUI = new CheckinUI(checkInControl);
+		
+		recordServiceControl = new RecordServiceCTL(hotel);
+		recordServiceControl.recordServiceUI = new RecordServiceUI(recordServiceControl);
 		
 		arrivalDate = format.parse("01-01-2001");
+		stayLength = 1;
+		roomType = RoomType.SINGLE;
+		roomId = 101;
+		cardType = CreditCardType.VISA;
+		guestName = "David";
+		guestAddress = "123 Fake St";
+		phoneNumber = 123456;
+		serviceType = ServiceType.BAR_FRIDGE;
+		cost = 15.00;
+		
+		creditCard = new CreditCard(cardType, AUTHORISED_CARD_NUMBER, ccv);
+		
+		checkInControl.hotel.addRoom(roomType, roomId);
+		room = checkInControl.hotel.findAvailableRoom(roomType, arrivalDate, stayLength);
+		guest = new Guest(guestName, guestAddress, phoneNumber);
+		confirmationNumber = checkInControl.hotel.book(room, guest, arrivalDate, stayLength, occupantNumber, creditCard);
+		
 	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
+	
 	@Test
-	void testServiceChargeZero() {
-		fail("Not yet implemented");
+	void testServiceChargeError() {
+		//arrange
+		checkInControl.confirmationNumberEntered(confirmationNumber);
+		checkInControl.checkInConfirmed(true);
+		
+		recordServiceControl.roomNumberEntered(roomId);
+		recordServiceControl.serviceDetailsEntered(serviceType, cost);
+		
+		//act
+		checkOutControl.state = CheckoutCTL.State.ROOM;
+		checkOutControl.roomIdEntered(roomId);
+		
 	}
-
 }
